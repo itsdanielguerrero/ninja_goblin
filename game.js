183b54b4c3b117cc91e6aea0163b63647101719e
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-10-20 20:36:54 -0400

    the start of a text based adventure game

---
+++ game.js
@@ -0,0 +1,15 @@
+function gamePlay () {
+    var quit = false //decalred a variable to track if player has quit game
+    var start //declared a variable to initiat game
+    console.log('Lets get it started')
+    if (start = 'y') {
+        while (quit == false){
+            quit = true
+        }
+    } else {
+        console.log('I see... there is no room for the weakhearted.')
+    }
+
+}
+
+gamePlay()
